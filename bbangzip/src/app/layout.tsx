68ddc7b445d37631081def20552aafb89f2edbc6
gomminjae <gommj0611@naver.com>
2024-09-22 22:04:40 +0900

    Feat: Next Foldering

--- bbangzip/src/app/layout.tsx
+++ bbangzip/src/app/layout.tsx
@@ -24,12 +24,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <html>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li><a href="/read/1">html</a></li>
+        <li><a href="/read/2">css</a></li>
+      </ol>
+      {children}
+      <ul>
+        <li><a href="/create">create</a></li>
+        <li><a href="/update/id">update</a></li>
+        <li><button>delete</button></li>
+      </ul>
+    </body>
+  </html>
   );
 }
